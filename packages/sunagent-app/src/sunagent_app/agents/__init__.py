e9ddc2bd38a04b3d1f87d04e608d6a0090512004
bobo <bobo@bobodeMacBook-Pro.local>
2025-12-05 10:46:02 +0800

    feat： 兼容gpt image

--- packages/sunagent-app/src/sunagent_app/agents/__init__.py
+++ packages/sunagent-app/src/sunagent_app/agents/__init__.py
@@ -1,10 +1,10 @@
 from ._context_builder_agent import ContextBuilderAgent, MentionStream
 from ._image_generate_agent import ImageGenerateAgent
 from ._steemit_context_builder_agent import SteemContextBuilder
+from ._storm_agent import StormAgent, StormConfig
 from ._token_launch_agent import TokenLaunchAgent
 from ._tweet_analysis_agent import TweetAnalysisAgent
 from ._tweet_check_agent import TweetCheckAgent
-from ._storm_agent import StormAgent, StormConfig
 
 __all__ = [
     "ContextBuilderAgent",
